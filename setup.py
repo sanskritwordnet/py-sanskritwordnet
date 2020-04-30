d13108d457bcce27fae4e98619ac1f255dbff7f9
ws308 <w.short@exeter.ac.uk>
2020-04-30 14:27:25 +0100

    fix CI/CD

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name="sanskritwordnet",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(),
     url="",
     license="GNU General Public License v3.0",
